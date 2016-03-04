9820b30c184ba2bbde30297108b7369cc63f247b
Zachariah Toulouse <ztoulouse1@gmail.com>
2016-03-04 16:19:25 -0500

    Made changes to dice, added logic

--- Scripts/scenes/play.js
+++ Scripts/scenes/play.js
@@ -19,16 +19,67 @@ var scenes;
         // PUBLIC METHODS +++++++++++++++++++++
         // Start Method
         Play.prototype.start = function () {
-            //Add Play Label
-            this._playLabel = new objects.Label("PLAY SCENE", "60px Consolas", "#000000", config.Screen.CENTER_X, config.Screen.CENTER_Y);
-            this.addChild(this._playLabel);
             //Add Play Button
-            this._playButton = new objects.Button;
+            this._rollButton = new objects.Button("RollButton", 400, 400);
+            this.addChild(this._rollButton);
+            this._rollButton.on("click", this._rollButtonClick, this);
+            //Initialize Array of Bitmaps
+            this._initializeBitmapArray();
             // add this scene to the global stage container
             stage.addChild(this);
         };
         // PLAY Scene updates here
         Play.prototype.update = function () {
+        };
+        //PRIVATE METHODS
+        Play.prototype._checkRange = function (value, lowerBounds, upperBounds) {
+            return (value >= lowerBounds && value <= upperBounds) ? value : -1;
+        };
+        Play.prototype._initializeBitmapArray = function () {
+            this._dice = new Array();
+            for (var die = 0; die < 2; die++) {
+                this._dice[die] = new createjs.Bitmap(assets.getResult("FirstDie"));
+                this._dice[die].x = 150 + (die * 230);
+                this._dice[die].y = 165;
+                this.addChild(this._dice[die]);
+                console.log("die" + die + " " + this._dice[die]);
+            }
+        };
+        Play.prototype._generateDice = function () {
+            var dices = [" ", " "];
+            var outcome = [0, 0, 0];
+            for (var spin = 0; spin < 2; spin++) {
+                outcome[spin] = Math.floor((Math.random() * 100) + 1);
+                switch (outcome[spin]) {
+                    case this._checkRange(outcome[spin], 1, 16):
+                        dices[spin] = "FirstDie";
+                        break;
+                    case this._checkRange(outcome[spin], 17, 33):
+                        dices[spin] = "SecondDie";
+                        break;
+                    case this._checkRange(outcome[spin], 34, 50):
+                        dices[spin] = "ThirdDie";
+                        break;
+                    case this._checkRange(outcome[spin], 51, 67):
+                        dices[spin] = "FourthDie";
+                        break;
+                    case this._checkRange(outcome[spin], 68, 84):
+                        dices[spin] = "FifthDie";
+                        break;
+                    case this._checkRange(outcome[spin], 85, 100):
+                        dices[spin] = "SixthDie";
+                        break;
+                }
+            }
+            return dices;
+        };
+        //EVENT HANDLERS ++++++++++++++++++++
+        Play.prototype._rollButtonClick = function (event) {
+            // generate 2 images
+            var bitmap = this._generateDice();
+            for (var reel = 0; reel < 3; reel++) {
+                this._dice[reel].image = assets.getResult(bitmap[reel]);
+            }
         };
         return Play;
     })(objects.Scene);
